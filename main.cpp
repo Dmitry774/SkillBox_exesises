2a11e9626ba520091e4fffe7d5513619006c5e62
Dmitry774 <fdmitrij1995@gmail.com>
2024-11-06 23:50:25 +0500

    1
    06/11/2024 Код из примера

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <matrix.h>
+#include <iostream>
+int main()
+{
+math::Matrix m (3, 3);
+m.print();
+
+std::cout << "Element at pos (1,1) = "<< m(1, 1) << std::endl;
+m(1,1) = 12.0;
+
+std::cout << std::endl;
+m.print();
+
+return 0;
+}
